9ffbedf9e4e573469c0fbfed657f9a6a453355aa
Vladislav Bedilo <v.overdrive@gmail.com>
2019-03-01 09:09:47 +0300

    add empty protection

---
+++ ProtectClient/Core/Protect/BaseProtect.cs
@@ -0,0 +1,8 @@
+namespace ProtectClient.Core.Protect
+{
+    public abstract class BaseProtect
+    {
+        public abstract void init();
+        public abstract bool validate();
+    }
+}
---
+++ ProtectClient/Core/Protect/KeyProtect.cs
@@ -0,0 +1,19 @@
+namespace ProtectClient.Core.Protect
+{
+    public class KeyProtect:  BaseProtect
+    {
+        public KeyProtect()
+        {
+        }
+
+        public override void init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool validate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
