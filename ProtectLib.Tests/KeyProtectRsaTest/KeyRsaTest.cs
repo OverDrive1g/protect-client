f7caa00db64fd1eeb4fc47c1259e051a069a80eb
Vladislav Bedilo <v.overdrive@gmail.com>
2019-03-02 21:25:51 +0300

    Тест для проверки метода

--- ProtectLib.Tests/KeyProtectRsaTest/KeyRsaTest.cs
+++ ProtectLib.Tests/KeyProtectRsaTest/KeyRsaTest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProtectClient.Core.Protect;
 
@@ -7,19 +10,54 @@ namespace ProtectLib.Tests.KeyProtectRsaTest
     [TestClass]
     public class KeyRsaTest
     {
+        public byte[] Encrypt(byte[] data, RSAParameters rsaKey, bool doPadding)
+        {
+            try
+            {
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.ImportParameters(rsaKey);
+                var endData = rsa.Encrypt(data, doPadding);
+                return endData;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public byte[] Decrypt(byte[] data, RSAParameters rsaKey, bool doPadding)
+        {
+            try
+            {   
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.ImportParameters(rsaKey);
+                var decryptData = rsa.Decrypt(data, doPadding);
+                return decryptData;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        
         [TestMethod]
         public void endTest()
         {
             String textToEncrypt = "1 - 5u>0uaW2";
             
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
             KeyProtectRsa keyProtect = new KeyProtectRsa();
-            var productNum = keyProtect.getProductNum();
+            var productNum = keyProtect.getPublicKey();
             
-            //создаеться криптопровайдер и зашифровывается textToEncrypt
-            //зашифрованый текст передается в класс
-            var encryptedText = "";
-            var result = keyProtect.activate(encryptedText);
+            rsa.ImportParameters(productNum);
 
+            var dataToEncrypt = Encoding.Unicode.GetBytes(textToEncrypt);
+            var encryptedData = rsa.Encrypt(dataToEncrypt, false);
+            
+            var result = keyProtect.activate(encryptedData);
+            
             Assert.IsTrue(result);
         }
     }
