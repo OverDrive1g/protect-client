b6d45245ef191d865d3b31d467edbc80756ba8f9
Vladislav Bedilo <v.overdrive@gmail.com>
2019-03-20 00:56:58 +0300

    Добавил новый класс защиты

---
+++ ProtectLib/Protect/RegProtect.cs
@@ -0,0 +1,18 @@
+namespace ProtectLib.Protect
+{
+    /// <summary>
+    /// Класс проверки регистра
+    /// </summary>
+    public class RegProtect: IBaseProtect
+    {
+        public void init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool validate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
