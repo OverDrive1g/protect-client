5e4ebd3e65817a2208e744eecddd859ceedcc561
Vladislav Bedilo <v.overdrive@gmail.com>
2019-03-20 00:57:13 +0300

    Добавил шаблон теста

---
+++ ProtectLib.Tests/RenameProtectTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProtectLib.Tests
+{
+    [TestClass]
+    public class RenameProtectTest
+    {
+        [TestInitialize]
+        public void testInitialize()
+        {
+            
+        }
+
+        [TestMethod]
+        public void none()
+        {
+        }
+    }
+}
