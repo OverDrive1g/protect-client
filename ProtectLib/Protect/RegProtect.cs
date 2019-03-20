d9c7fb5a87f700e6ba41b96e3d02b4be0fb5830f
Vladislav Bedilo <v.overdrive@gmail.com>
2019-03-20 23:11:54 +0300

    Реализовал логику "защиты" через регистр

--- ProtectLib/Protect/RegProtect.cs
+++ ProtectLib/Protect/RegProtect.cs
@@ -1,3 +1,6 @@
+using Microsoft.Win32;
+using ProtectLib.Exception;
+
 namespace ProtectLib.Protect
 {
     /// <summary>
@@ -5,23 +8,46 @@ namespace ProtectLib.Protect
     /// </summary>
     public class RegProtect: IBaseProtect
     {
-       
+        private readonly string _regName;
+        private RegistryKey localMachineKey;
         public RegProtect(string regName)
         {
+            _regName = regName;
+            localMachineKey = Registry.LocalMachine;
         }
 
         public void init()
         {
+            if (isInit())
+            {
+                return;
+            }
+
+            RegistryKey currentUserKey = Registry.CurrentUser;
+
+            var programKey = currentUserKey.CreateSubKey(_regName);
+
+            programKey.SetValue("initial_date", "01-01-2001");
+            programKey.SetValue("password", "12345");
+            programKey.Close();
         }
 
         public bool validate()
         {
-            return false;
+            if (!isInit())
+            {
+                throw new NotInitProtectException();
+            }
+
+            return true;
         }
 
         private bool isInit()
         {
-            return false;
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            var programKey = currentUserKey.OpenSubKey(_regName);
+
+            return programKey != null;
         }
     }
 }
