987a2f783ea50ad134fbd0473950011facb5c1dc
Vladislav Bedilo <v.overdrive@gmail.com>
2019-04-07 16:23:06 +0300

    Добавил еще один способ хранения

---
+++ ProtectLib/Storage/InMemoryStorage.cs
@@ -0,0 +1,30 @@
+namespace ProtectLib.Storage
+{
+    public class InMemoryStorage:IStorage
+    {
+        public void init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object getValue(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void getAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void save()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
