a634bc81bd0ba350aafab25e8a59d67e344b350d
Vladislav Bedilo <v.overdrive@gmail.com>
2019-04-14 12:21:17 +0300

    Начал реализовывать класс хранения в регистре

--- ProtectLib/Storage/RegStorage.cs
+++ ProtectLib/Storage/RegStorage.cs
@@ -1,6 +1,30 @@
 namespace ProtectLib.Storage
 {
-    public class RegStorage
+    public class RegStorage<T>:IStorage<T>
     {
+        public void init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void set(T value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void update()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void save()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
