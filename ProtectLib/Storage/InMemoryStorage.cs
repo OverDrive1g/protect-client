fb15e3d355ae90bb6d78615acf639315a8ab7ff4
Vladislav Bedilo <v.overdrive@gmail.com>
2019-04-14 12:16:39 +0300

    реализовал хранилище в памяти

--- ProtectLib/Storage/InMemoryStorage.cs
+++ ProtectLib/Storage/InMemoryStorage.cs
@@ -1,6 +1,32 @@
 namespace ProtectLib.Storage
 {
-    public class InMemoryStorage
+    public class InMemoryStorage<T> : IStorage<T>
     {
+        private T _payload;
+
+        public void init()
+        {
+            // нет необходимости реализовывать, так как все в памяти
+        }
+
+        public T get()
+        {
+            return _payload;
+        }
+
+        public void set(T value)
+        {
+            _payload = value;
+        }
+
+        public void update()
+        {
+            // нет необходимости реализовывать, так как все в памяти
+        }
+
+        public void save()
+        {
+            // нет необходимости реализовывать, так как все в памяти
+        }
     }
 }
