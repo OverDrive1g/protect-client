094a117ae333a1cddb04c1acedb73b91a522a318
Vladislav Bedilo <v.overdrive@gmail.com>
2019-04-14 12:04:51 +0300

    Добавил тест

---
+++ ProtectLib.Tests/Storage/FileStorageTest.cs
@@ -0,0 +1,36 @@
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProtectLib.Storage;
+
+namespace ProtectLib.Tests.Storage
+{
+    [TestClass]
+    public class FileStorageTest
+    {
+        [TestInitialize]
+        public void testInitialize()
+        {
+        }
+
+        [TestMethod]
+        public void get_withSet_true()
+        {
+            var payload = new Payload {ID = 1, Name = "test"};
+            var folderName = "testFolder";
+            var fileName = "testFile";
+            var fileFormatter = new BinaryFormatter();
+            var fileStorage = new FileStorage<Payload>(folderName, fileName, fileFormatter);
+            
+            fileStorage.set(payload);
+            var storagePayload = fileStorage.get();
+
+            Assert.AreEqual(storagePayload, payload);
+        }
+    }
+
+    public class Payload
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
