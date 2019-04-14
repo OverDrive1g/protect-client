5ab5d0476e939935b9feb29995b35f9bd1e45005
Vladislav Bedilo <v.overdrive@gmail.com>
2019-04-14 22:14:00 +0300

    Реализовал хранилище в регистре

--- ProtectLib/Storage/RegStorage.cs
+++ ProtectLib/Storage/RegStorage.cs
@@ -1,30 +1,67 @@
+using System.Web.Script.Serialization;
+using Microsoft.Win32;
+
 namespace ProtectLib.Storage
 {
     public class RegStorage<T>:IStorage<T>
     {
+        private string _registerName;
+        private T _payload;
+
+        public RegStorage(string registerName)
+        {
+            _registerName = registerName;
+        }
+
         public void init()
         {
-            throw new System.NotImplementedException();
+            if (!IsInit())
+            {
+                RegistryKey currentUserKey = Registry.CurrentUser;
+
+                var programKey = currentUserKey.CreateSubKey(_registerName);
+                programKey?.Close();
+            }
         }
 
         public T get()
         {
-            throw new System.NotImplementedException();
+            return _payload;
         }
 
         public void set(T value)
         {
-            throw new System.NotImplementedException();
+            _payload = value;
         }
 
         public void update()
         {
-            throw new System.NotImplementedException();
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            var programKey = currentUserKey.OpenSubKey(_registerName);
+            programKey.SetValue("main", GetPayload());
+            programKey.Close();
         }
 
         public void save()
         {
-            throw new System.NotImplementedException();
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            var programKey = currentUserKey.OpenSubKey(_registerName);
+            var payload = programKey.GetValue("main");
+            
+            _payload = new JavaScriptSerializer().Deserialize<T>((string)payload);
+        }
+
+        private bool IsInit()
+        {
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            var programKey = currentUserKey.OpenSubKey(_registerName);
+
+            return programKey != null;
+        }
+
+        private string GetPayload()
+        {
+            return new JavaScriptSerializer().Serialize(_payload);
         }
     }
 }
